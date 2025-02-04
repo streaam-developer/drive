779eff230ad750165d9ec2fa33a645a323db0a88
streaam-developer <163761596+streaam-developer@users.noreply.github.com>
2025-02-04 08:44:54 +0530

    Update authorize.py

--- bot/plugins/authorize.py
+++ bot/plugins/authorize.py
@@ -14,7 +14,7 @@ from bot.helpers.utils import CustomFilters
 
 
 OAUTH_SCOPE = "https://www.googleapis.com/auth/drive"
-REDIRECT_URI = "https://elbots.netlify.app/gdriveauth"
+REDIRECT_URI = "https://verify.axhad.workers.dev/"
 
 flow = None
 
